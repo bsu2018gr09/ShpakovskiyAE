608cb5801a13e4325fcd75e6399173e223ad0fca
AndreyShpakovskiy <43923078+AndreyShpakovskiy@users.noreply.github.com>
2019-03-16 16:12:11 +0300

    Update 0-2.cpp

--- 0-2.cpp
+++ 0-2.cpp
@@ -1,1 +1,59 @@
-﻿
+﻿#include<iostream>
+#include<time.h>
+using namespace std;
+void randInitArrFrom0To10(int *A, int N);
+void findingRepeatingElements(int *A, int N);
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int N;
+	cout << "Введите длину массива: ";
+	cin >> N;
+	int *A = new int[N];
+	if (!A)
+	{
+		cout << "Ошибка!\n";
+		system("pause");
+		return 0;
+	}
+	//randInitArrFrom0To10(A, N);
+	for (int i = 0; i < N; i++) 
+	{
+		cin >> *(A + i);
+	}
+	findingRepeatingElements(A, N);
+	delete[] A;
+	A = nullptr;
+	system("pause");
+}
+void randInitArr(int *A, int N) 
+{
+		srand(time(0));
+		for (int i = 0; i < N; ++i) {
+			*(A+i) = rand() % 10;
+		}
+}
+void findingRepeatingElements(int *A, int N)
+{
+	int k = 0,kmax=0;
+		for (int i = 1; i < N; i++)
+		{
+			if (*(A + i) == *(A + i - 1))
+				++k;
+			else
+			{
+				if (k >= kmax)
+				{
+					kmax = k;
+				}
+				k = 0;
+			}	
+		
+		}
+		if (k >= kmax)
+		{
+			kmax = k;
+		}
+		cout << kmax+1;
+
+}
