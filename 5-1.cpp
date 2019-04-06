c824d7940c9cc0237d4d59bb995ea5d168ef2c75
AndreyShpakovskiy <43923078+AndreyShpakovskiy@users.noreply.github.com>
2019-04-06 19:25:33 +0300

    Извините, случайно отправил не тот файл

--- 5-1.cpp
+++ 5-1.cpp
@@ -1,15 +1,16 @@
-#include "pch.h"
-#include<iostream> //РіРґРµ СѓСЃР»РѕРІРёРµ Р·Р°РґР°С‡Рё? РљР°РєРѕР№ РіСЂР°РґРёРµРЅС‚ С‚С‹ РґРµР»Р°РµС€СЊ??? РЎРѕРІРїР°РґР°РµС‚ РєРѕРґ СЃ Р–РёР»РєРѕ
-#include<fstream>
+//Горизонтальный градиент c выбранным начальным и конечным цветом
+#include<iostream>
+#include<cmath>
+#include<fstream> 		                 
 using namespace std;
 struct color { unsigned char r; unsigned char g; unsigned char b; } c;
 
 int main() {
+	setlocale(LC_ALL, "Russian");
 	ifstream fff("d:\\1.bmp", ios::binary);
 	ofstream ggg("d:\\rez.bmp", ios::binary);
-
-	if (!fff) { cout << "No file d:\\1.bmp. Can't open\n"; exit(1); }
-	if (!ggg) { cout << " file d:\\rez.bmp. Can't create\n"; exit(1); }
+	if (!fff) { cout << "No file C:\\file.bmp. Can't open\n"; exit(1); }
+	if (!ggg) { cout << "No file C:\\rez.bmp. Can't create\n"; exit(1); }
 
 	char buf[30];
 	unsigned char r, g, b;
@@ -18,28 +19,49 @@ int main() {
 
 	fff.read((char *)&buf, 18);
 	ggg.write((char *)&buf, 18);
-
-	w = 128;
-	ggg.write((char *)&w, 4);
-	h = 256;
-	ggg.write((char *)&h, 4);
+	fff.read((char *)&w, 4); 
+	cout << "w=" << w;
+	fff.read((char *)&w, 4);
+	cout << ", h=" << w;
+	w = 256; ggg.write((char *)&w, 4);
+	h = 256; ggg.write((char *)&h, 4);
 	fff.read((char *)&buf, 28);
 	ggg.write((char *)&buf, 28);
 
-	c.r = 128; c.g = 16; c.b = 77;
-	double dr = (255 - 128) / 80;
-	double dg = (49 - 16) / 80;
-	double db = (99 - 77) / 80;
-	for (int i = 1; i <= h; ++i) {
-		for (int j = 1; j <= w; ++j) {
+	float step;
+	cout << '\n' << "step: ";
+	cin >> step;
+	unsigned int rStart, gStart, bStart;
+	cout << "Введите r, g и b начального цвета: ";
+	cin >> rStart;
+	cin >> gStart; 
+	cin >> bStart;
+	c.r = rStart;
+	c.g = gStart;
+	c.b = bStart;
+	float rEnd = 0, gEnd = 0, bEnd = 0;
+	cout << "Введите r, g и b конечного цвета: ";
+	cin >> rEnd;
+	cin >> gEnd;
+	cin >> bEnd;
+	float stepR = abs(rEnd - rStart) / step;
+	float stepG = abs(gEnd - gStart) / step;
+	float stepB = abs(bEnd - bStart) / step;
+	int k = 0;
+
+	for (int i{ 0 }; i < w; ++i) { 
+		for (int j{ 0 }; j < h; ++j) {
+			if (k < step) {
+				c.b += stepB;
+				c.g += stepG;
+				c.r += stepR;
+				k++;
+			}
 			ggg.write((char *)&c, 3);
-			c.b = c.b + db;
-			c.g = c.g + dg;
-			c.r = c.r + dr;
 		}
-		c.r = 128; c.g = 16; c.b = 77;
+		c.r = rStart;
+		c.g = gStart;
+		c.b = bStart;
+		k = 0;
 	}
-	fff.close();
-	ggg.close();
-	return 1;
 }
