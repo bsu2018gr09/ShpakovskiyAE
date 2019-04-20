7a4e07abd43f918998efc7621790e4ef9e5e7cfa
barvenov <barvenovsa@gmail.com>
2019-04-20 21:27:04 +0200

    Пока плохо...

--- 6-1.cpp
+++ 6-1.cpp
@@ -40,20 +40,20 @@ public:
 		cout << "B:" << "(" << bx << ";" << by << ")\n";
 		cout << "C:" << "(" << cx << ";" << cy << ")\n";
 	};
-	float sideLngth(float x1, float y1, float x2, float y2)
+	float sideLngth(float x1, float y1, float x2, float y2)// совсем плохо!!!! откуда столько параметров???? я хочу узнать у ТРЕУГОЛЬНИКА что то 
 	{
 		float length;
-		length = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+		length = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));//Ой, что за код???? Ужас!!!! Где оптимизация???? зачем pow
 		return length;
 	};
-	float perimeter(float x1, float y1, float x2, float y2, float x3, float y3)
+	float perimeter(float x1, float y1, float x2, float y2, float x3, float y3)// совсем плохо!!!! откуда столько параметров???? я хочу узнать у ТРЕУГОЛЬНИКА что то 
 	{
 		float p;
 		
 		p = sideLngth(x1,y1,x2,y2) + sideLngth(x1, y1, x3, y3) + sideLngth(x2, y2, x3, y3);
 			return p;
 	};
-	float square(float x1, float y1, float x2, float y2, float x3, float y3)
+	float square(float x1, float y1, float x2, float y2, float x3, float y3)//что за дурное название???? // совсем плохо!!!! откуда столько параметров???? я хочу узнать у ТРЕУГОЛЬНИКА что то 
 	{
 		float s;
 		float p = perimeter(x1, y1, x2, y2, x3, y3) / 2;
