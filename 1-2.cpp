c1882b8720b55a385f52904a2014a86345c549b6
AndreyShpakovskiy <43923078+AndreyShpakovskiy@users.noreply.github.com>
2019-05-31 10:53:26 +0300

    Add files via upload

--- 1-2.cpp
+++ 1-2.cpp
@@ -1,11 +1,13 @@
-/*Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования.
+﻿/*Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования.
  Отрицательные элементы расположить в порядке убывания. Дополнительный массив не использовать.*/
 #include<iostream>
 using namespace std;
 void fillArr(int*A, int N);
 void freeMemory(int*A);
 int *InitArr(int N) {
-	int *A = new int[N];//нет проверки
+	int *A = new int[N];
+	if (!A)
+		cout << "error";
 	return A;
 }
 void posNegSort(int*A, int N);
@@ -26,7 +28,7 @@ int main()
 }
 void freeMemory(int*A)
 {
-	delete[]A;//нет nullptr
+	delete[]A;
 }
 void fillArr(int*A, int N)
 {
@@ -35,19 +37,25 @@ void fillArr(int*A, int N)
 }
 void posNegSort(int*A, int N)
 {
+	int flag{ 0 };
 	int k{ 0 };
 	for (int i = N-1;i >=0;i--)
 	{
 		if (*(A + i) >= 0)
 			swap(*(A + i), *(A + N - 1 - k++));
 	} 
-	for (int i = 0; i < N - 1-k; i++)
-	{
-		for (int j = 0; j < N - i - 1-k; j++)
-		{
-			if (*(A + j) < *(A + j + 1))
-				swap(*(A + j), *(A + j + 1));//очень наивная сортировка без малейшей оптимизации  
+	for (int i = 0; i < N; ++i) {
+		flag = 0;
+		for (int j = 0; j < N; ++j) {
+			if (*(A + j) < *(A + j + 1)) {
+				if (*(A + j + 1) <= 0) {
+					swap(*(A + j), *(A + j + 1));
+					flag = 1;
+				}
+			}
 		}
+		if (!flag)
+			break;
 	}
 }
 void printArr(int*A, int N)
